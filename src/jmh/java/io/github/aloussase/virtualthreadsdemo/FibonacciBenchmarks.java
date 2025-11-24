449f9d8f892a4c7ac657888051bbc0301dd36b6a
Alexander Goussas <goussasalexander@gmail.com>
2025-11-24 11:41:30 -0500

    refactor fibonacci benchmarks

--- src/jmh/java/io/github/aloussase/virtualthreadsdemo/FibonacciBenchmarks.java
+++ src/jmh/java/io/github/aloussase/virtualthreadsdemo/FibonacciBenchmarks.java
@@ -9,23 +9,43 @@ import java.util.concurrent.TimeUnit;
 // Virtual threads cannot be woken up when all carrier threads are busy
 // If my CPU has 4 cores, then at most 4 virtual threads can be running at the same time.
 // Performance in VTs comes from cooperatively yielding while waiting for IO.
-@Threads(1)
+@Threads(4)
+@Warmup(iterations = 1)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.SingleShotTime)
 public class FibonacciBenchmarks {
 
 
     /**
      * Number of iterations.
-     * <p>
-     * At a low number of iterations (e.g. 100, 1000) the overhead of thread creation overwhelms the
-     * execution time.
      */
-    private final static int N = 500_000;
+    private final static int N = 100_000_000;
 
-    @Warmup(iterations = 1)
     @Benchmark
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @BenchmarkMode(Mode.SingleShotTime)
-    public void fibonacciSequential(Blackhole blackhole) {
+    public void sequential(Blackhole blackhole) {
+        blackhole.consume(invoke());
+        blackhole.consume(invoke());
+    }
+
+    @Benchmark
+    public void concurrent(Blackhole blackhole) throws InterruptedException {
+        final var th1 = Thread.ofVirtual().start(() -> blackhole.consume(invoke()));
+        final var th2 = Thread.ofVirtual().start(() -> blackhole.consume(invoke()));
+        th1.join();
+        th2.join();
+    }
+
+    @Benchmark
+    public void parallel(Blackhole blackhole) throws InterruptedException {
+        final var th1 = new Thread(() -> blackhole.consume(invoke()));
+        final var th2 = new Thread(() -> blackhole.consume(invoke()));
+        th1.start();
+        th2.start();
+        th1.join();
+        th2.join();
+    }
+
+    private int invoke() {
         int a = 0;
         int b = 1;
         for (int i = 2; i < N; i++) {
@@ -33,68 +53,7 @@ public class FibonacciBenchmarks {
             a = b;
             b = c;
         }
-        blackhole.consume(b);
-    }
-
-    @Warmup(iterations = 2)
-    @Benchmark
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @BenchmarkMode(Mode.SingleShotTime)
-    public void fibonacciVirtualThreads(Blackhole blackhole) throws InterruptedException {
-        final var th1 = Thread.ofVirtual().start(() -> {
-            int a = 0;
-            int b = 1;
-            for (int i = 2; i < N / 2; i++) {
-                int c = a + b;
-                a = b;
-                b = c;
-            }
-            blackhole.consume(b);
-        });
-        final var th2 = Thread.ofVirtual().start(() -> {
-            int a = 0;
-            int b = 1;
-            for (int i = 2; i < N / 2; i++) {
-                int c = a + b;
-                a = b;
-                b = c;
-            }
-            blackhole.consume(b);
-        });
-
-        th1.join();
-        th2.join();
-    }
-
-    @Warmup(iterations = 2)
-    @Benchmark
-    @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    @BenchmarkMode(Mode.SingleShotTime)
-    public void fibonacciPlatformThreads(Blackhole blackhole) throws InterruptedException {
-        final var th1 = new Thread(() -> {
-            int a = 0;
-            int b = 1;
-            for (int i = 2; i < N / 2; i++) {
-                int c = a + b;
-                a = b;
-                b = c;
-            }
-            blackhole.consume(b);
-        });
-        final var th2 = new Thread(() -> {
-            int a = 0;
-            int b = 1;
-            for (int i = 2; i < N / 2; i++) {
-                int c = a + b;
-                a = b;
-                b = c;
-            }
-            blackhole.consume(b);
-        });
-        th1.start();
-        th2.start();
-        th1.join();
-        th2.join();
+        return b;
     }
 
 }
